fec7ff76928045fa3707a44661586f7131b2eebb
Nikolay Tyukavkin <tyukavkin@wiley.com>
2017-01-13 16:52:34 +0300

    add testRailId to @TestLink

--- src/main/java/com/wiley/autotest/annotations/TestLink.java
+++ src/main/java/com/wiley/autotest/annotations/TestLink.java
@@ -14,7 +14,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({METHOD})
 public @interface TestLink {
+
     String rationalId() default "";
+
+    String testRailId() default "";
 
     String testLinkId() default "";
 
