6164916a34866f4f1e78d1584ed615ca934e0946
Nikolay Tyukavkin <tyukavkin@wiley.com>
2017-06-07 10:45:32 +0300

    merge from selenium3 anm mobile

--- src/test/java/com/wiley/autotest/framework/tests/drivers/GridConfig.java
+++ src/test/java/com/wiley/autotest/framework/tests/drivers/GridConfig.java
@@ -1,12 +1,41 @@
 package com.wiley.autotest.framework.tests.drivers;
 
+import com.wiley.autotest.framework.config.BaseTest;
+import com.wiley.autotest.services.Configuration;
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.internal.utils.SelfRegisteringRemote;
+import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
+import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
+import org.openqa.grid.web.Hub;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
 /**
  * User: ntyukavkin
- * Date: 07.06.2017
- * Time: 10:41
- * Test Case modified date:
- * Preconditions:
- * Description:
+ * Date: 31.05.2017
+ * Time: 11:50
  */
-public class GridConfig {
+public class GridConfig extends BaseTest {
+
+    //Start grid hub and node for test
+    @Autowired
+    private void setConfiguration(Configuration configuration) throws Exception {
+        GridHubConfiguration config = GridHubConfiguration.loadFromJSON(getClass().getResource("/grid/hubConfig.json").getPath());
+
+        Hub hub = new Hub(config);
+        hub.getConfiguration();
+        hub.start();
+
+        RegistrationRequest req = new RegistrationRequest(GridNodeConfiguration.loadFromJSON(getClass().getResource("/grid/node.json").getPath()));
+
+        SelfRegisteringRemote remote = new SelfRegisteringRemote(req);
+        remote.startRegistrationProcess();
+    }
+
+    @Test
+    public void grid_config_test() throws Exception {
+        openPage("main.html");
+
+        System.out.println("");
+    }
 }
