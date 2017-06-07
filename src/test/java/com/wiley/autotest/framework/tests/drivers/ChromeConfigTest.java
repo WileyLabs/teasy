8ae66a92a14d82f37922a70091e5f931d94e97ea
Nikolay Tyukavkin <tyukavkin@wiley.com>
2017-06-07 10:43:10 +0300

    merge from selenium3 anm mobile

--- src/test/java/com/wiley/autotest/framework/tests/drivers/ChromeConfigTest.java
+++ src/test/java/com/wiley/autotest/framework/tests/drivers/ChromeConfigTest.java
@@ -1,9 +1,45 @@
 package com.wiley.autotest.framework.tests.drivers;
 
+import com.wiley.autotest.annotations.OurAfterMethod;
+import com.wiley.autotest.framework.config.BaseTest;
+import com.wiley.autotest.selenium.SeleniumHolder;
+import com.wiley.autotest.selenium.driver.FramesTransparentWebDriver;
+import com.wiley.autotest.services.Configuration;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * User: ntyukavkin
- * Date: 07.06.2017
- * Time: 10:37
+ * Date: 30.05.2017
+ * Time: 17:42
  */
-public class ChromeConfigTest {
+public class ChromeConfigTest extends BaseTest {
+
+    @Autowired
+    private void setConfiguration(Configuration configuration) {
+        getSettings().setDriverName("chrome");
+        //set value other than default
+        configuration.addCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+    }
+
+    @Test
+    public void chrome_config_test() {
+        openPage("main.html");
+        WebDriver webDriver = ((FramesTransparentWebDriver) SeleniumHolder.getWebDriver()).getWrappedDriver();
+
+        Assert.assertTrue(webDriver instanceof ChromeDriver);
+
+        Assert.assertEquals(((RemoteWebDriver) webDriver).getCapabilities().getCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR), UnexpectedAlertBehaviour.IGNORE.toString());
+    }
+
+    @OurAfterMethod
+    public void after() {
+        SeleniumHolder.getWebDriver().quit();
+    }
 }
---
+++ src/test/java/com/wiley/autotest/framework/tests/drivers/GridConfig.java
@@ -0,0 +1,12 @@
+package com.wiley.autotest.framework.tests.drivers;
+
+/**
+ * User: ntyukavkin
+ * Date: 07.06.2017
+ * Time: 10:41
+ * Test Case modified date:
+ * Preconditions:
+ * Description:
+ */
+public class GridConfig {
+}
