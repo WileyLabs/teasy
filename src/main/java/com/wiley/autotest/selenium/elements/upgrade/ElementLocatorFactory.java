d2451058b774a51f7ddd5bd230381328222c3cb1
Vladimir Efimov <vefimov@wiley.com>
2017-09-13 17:21:28 +0300

    fixed visibility scope for variables

--- src/main/java/com/wiley/autotest/selenium/elements/upgrade/ElementLocatorFactory.java
+++ src/main/java/com/wiley/autotest/selenium/elements/upgrade/ElementLocatorFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class ElementLocatorFactory {
+
     private TeasyElementData elementData;
     private WebDriver driver;
 
