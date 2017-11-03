0cbca3ada6fe4dd56ecd06b3ff0dd616ce2aa269
Vladimir Efimov <vefimov@wiley.com>
2017-11-03 11:27:27 +0300

    addd comment and a private constructor to ParamsHolder

--- src/main/java/com/wiley/autotest/services/ParamsHolder.java
+++ src/main/java/com/wiley/autotest/services/ParamsHolder.java
@@ -4,14 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * User: ntyukavkin
- * Date: 01.11.2017
- * Time: 15:19
+ * Class to transfer any data in scope of
+ * - test
+ * - group of tests (using testNG group)
+ * <p>
+ * As we adopt fluent approach to our PagObjects this class helps to retrieve data from page
+ * and then get it anywhere you need
+ * <p>
+ * Example:
+ * <p>
+ * In page class add a method like
+ * public SomePage setSomeData() {
+ *  ParamsHolder.setParameter("some.key", anyObjectFromPage);
+ *  return this;
+ * }
+ * <p>
+ * In test class (or wherever you need the data) simply call
+ * <p>
+ * Object objectFromPage = ParamsHolder.getParameter("some.key");
  */
 public class ParamsHolder {
 
     private static ThreadLocal<Map<String, Object>> paramMap = ThreadLocal.withInitial(HashMap::new);
     private static ThreadLocal<Map<String, Object>> paramMapForGroup = ThreadLocal.withInitial(HashMap::new);
+
+    private ParamsHolder() {
+    }
 
     public static void setParameter(String key, Object value) {
         paramMap.get().put(key, value);
