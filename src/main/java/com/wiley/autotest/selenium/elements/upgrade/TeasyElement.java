d2f6e711dd75b91e4eae62bd609ebfe63cdcaba0
Vladimir Efimov <vefimov@wiley.com>
2018-03-01 11:52:52 +0300

    #148. Added documentation for TeasyElement. Removed forgottent unnecesarry condition class

--- src/main/java/com/wiley/autotest/selenium/elements/upgrade/TeasyElement.java
+++ src/main/java/com/wiley/autotest/selenium/elements/upgrade/TeasyElement.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+/**
+ * Teasy representation of a {@link WebElement}
+ */
 public interface TeasyElement extends WebElement {
 
     /**
-     * Do not use this method. it does not fit TeasyElement concept and only here because
+     * Do not use this method. it does not fit TeasyElement concept and is only here because
      * it is originally a method of WebElement.
      * use elements() or domElements(), depending on your needs
      */
@@ -20,7 +23,7 @@ public interface TeasyElement extends WebElement {
     List<WebElement> findElements(By by);
 
     /**
-     * Do not use this method. it does not fit TeasyElement concept and only here because
+     * Do not use this method. it does not fit TeasyElement concept and is only here because
      * it is originally a method of WebElement
      * use element() or domElement() depending on your needs
      */
@@ -28,37 +31,164 @@ public interface TeasyElement extends WebElement {
     @Deprecated
     WebElement findElement(By by);
 
+    /**
+     * Gets the original web element
+     *
+     * @return pure {@link WebElement}
+     */
     WebElement getWrappedWebElement();
 
+    /**
+     * Tells you whether element is stale (i.e. was detached from DOM)
+     *
+     * @return true - in case of a stale element, false - otherwise
+     */
     boolean isStale();
 
+    /**
+     * Gets {@link Locator} interface
+     * Used for interactions with location of an element
+     *
+     * @return instance of {@link Locator}
+     */
     Locator getLocator();
 
+    /**
+     * Calls assertion engine with default settings
+     *
+     * @return instance of {@link Should}
+     */
     Should should();
 
+    /**
+     * Calls assertion engine with custom settings passed via {@link SearchStrategy}
+     *
+     * @param strategy - custom settings for assertion
+     * @return instance of {@link Should}
+     */
     Should should(SearchStrategy strategy);
 
+    /**
+     * Calls waiting engine with default settings
+     *
+     * @return instance of {@link ElementWaitFor}
+     */
     ElementWaitFor waitFor();
 
+    /**
+     * Calls waiting engine with custom settings passed via {@link SearchStrategy}
+     *
+     * @param strategy - custom settings for waiting
+     * @return instance of {@link ElementWaitFor}
+     */
     ElementWaitFor waitFor(SearchStrategy strategy);
 
+    /**
+     * Gets parent element
+     *
+     * @return - {@link TeasyElement}
+     */
     TeasyElement getParent();
 
+    /**
+     * Gets parent element of N-th level
+     * For the <el3> from below if we call getParent(2)
+     * will return TeasyElement for <el1>
+     * <el>
+     * <el1>
+     * <el2>
+     * <el3>example DOM structure</el3>
+     * </el2>
+     * </el1>
+     * </el>
+     *
+     * @param level - levels up of a parenting
+     * @return - {@link TeasyElement}
+     */
     TeasyElement getParent(int level);
 
+    /**
+     * Finds first visible element using locator {@link By}
+     * and default search strategy
+     *
+     * @param by - locator
+     * @return - first found visible element
+     */
     TeasyElement element(By by);
 
+    /**
+     * Finds first visible element using locator {@link By}
+     * and custom search strategy {@link SearchStrategy}
+     *
+     * @param by       - locator
+     * @param strategy - custom search strategy
+     * @return - {@link TeasyElement}
+     */
     TeasyElement element(By by, SearchStrategy strategy);
 
+    /**
+     * Finds first found visible elements using locator {@link By}
+     *
+     * @param by - locator
+     * @return - {@link TeasyElementList}
+     */
     TeasyElementList elements(By by);
 
+    /**
+     * Finds visible elements using locator {@link By}
+     * and custom search strategy {@link SearchStrategy}
+     *
+     * @param by       - locator
+     * @param strategy - custom search strategy
+     * @return - {@link TeasyElementList}
+     */
     TeasyElementList elements(By by, SearchStrategy strategy);
 
+    /**
+     * Finds first dom element using locator {@link By}
+     * note:
+     * dom element is just an element present in dom
+     * which is not necessarily visible
+     *
+     * @param by - locator
+     * @return - {@link TeasyElement}
+     */
     TeasyElement domElement(By by);
 
+    /**
+     * Finds first dom element using locator {@link By}
+     * and custom search strategy {@link SearchStrategy}
+     * note:
+     * dom element is just an element present in dom
+     * which is not necessarily visible
+     *
+     * @param by       - locator
+     * @param strategy - custom search strategy
+     * @return - {@link TeasyElement}
+     */
     TeasyElement domElement(By by, SearchStrategy strategy);
 
+    /**
+     * Finds first found dom elements using locator {@link By}
+     * note:
+     * dom element is just an element present in dom
+     * which is not necessarily visible
+     *
+     * @param by - locator
+     * @return {@link TeasyElement}
+     */
     TeasyElementList domElements(By by);
 
+    /**
+     * Finds dom elements using locator {@link By}
+     * and custom search strategy {@link SearchStrategy}
+     * note:
+     * dom element is just an element present in dom
+     * which is not necessarily visible
+     *
+     * @param by       - locator
+     * @param strategy - custom search strategy
+     * @return
+     */
     TeasyElementList domElements(By by, SearchStrategy strategy);
 }
--- src/main/java/com/wiley/autotest/selenium/elements/upgrade/TeasyElementList.java
+++ src/main/java/com/wiley/autotest/selenium/elements/upgrade/TeasyElementList.java
@@ -1,16 +1,19 @@
 package com.wiley.autotest.selenium.elements.upgrade;
 
 import com.wiley.autotest.selenium.SeleniumHolder;
-import com.wiley.autotest.selenium.elements.upgrade.should.*;
+import com.wiley.autotest.selenium.elements.upgrade.should.GeneralListShould;
+import com.wiley.autotest.selenium.elements.upgrade.should.ListShould;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Represents list of TeasyElements adding elements-related behavior to standard ArrayList
  */
 public class TeasyElementList extends ArrayList<TeasyElement> {
 
+    //This constructor is needed for better streams api support
     public TeasyElementList() {
         super();
     }
