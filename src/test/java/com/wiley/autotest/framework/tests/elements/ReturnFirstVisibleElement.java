53d22a3882cd022828fe3442c0011e53c20d136d
Nikolay Tyukavkin <tyukavkin@wiley.com>
2018-04-09 11:45:32 +0300

    delete fail from test

--- src/test/java/com/wiley/autotest/framework/tests/elements/ReturnFirstVisibleElement.java
+++ src/test/java/com/wiley/autotest/framework/tests/elements/ReturnFirstVisibleElement.java
@@ -10,6 +10,5 @@ public class ReturnFirstVisibleElement extends BaseTest {
     public void test() {
         openPage("mainTestElement.html", TestElementPage.class)
                 .checkFirstVisibleElement();
-        setPostponedTestFail("asd");
     }
 }
