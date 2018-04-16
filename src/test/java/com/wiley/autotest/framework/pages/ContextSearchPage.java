635fe34b5e705eed305a9595d31939b8e551f946
Vladimir Efimov <vefimov@wiley.com>
2018-04-16 11:16:33 +0300

    fixed assertion imports. removd outdated test

--- src/test/java/com/wiley/autotest/framework/pages/ContextSearchPage.java
+++ src/test/java/com/wiley/autotest/framework/pages/ContextSearchPage.java
@@ -2,6 +2,7 @@ package com.wiley.autotest.framework.pages;
 
 import com.wiley.autotest.selenium.context.AbstractPage;
 import com.wiley.autotest.selenium.elements.upgrade.TeasyElement;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ public class ContextSearchPage extends AbstractPage {
 
     public ContextSearchPage checkContextSearch() {
         List<TeasyElement> elements = element(By.cssSelector(".test")).elements(By.cssSelector(".contextTest"));
-        assertEquals(elements.size(), 1);
+        Assertions.assertThat(elements.size()).isEqualTo(1);
         elements.get(0).should().haveText("Hello context");
         return this;
     }
--- src/test/java/com/wiley/autotest/framework/pages/SelectPage.java
+++ src/test/java/com/wiley/autotest/framework/pages/SelectPage.java
@@ -2,6 +2,7 @@ package com.wiley.autotest.framework.pages;
 
 import com.wiley.autotest.selenium.context.AbstractPage;
 import com.wiley.autotest.selenium.elements.upgrade.TeasyElement;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ public class SelectPage extends AbstractPage {
 
     public SelectPage checkOptionsText() {
         List<TeasyElement> options = select(By.tagName("select")).getOptions();
-        assertEquals(options.size(), 4);
+        Assertions.assertThat(options.size()).isEqualTo(4);
         options.get(0).should().haveText("Volvo");
         options.get(1).should().haveText("Saab");
         options.get(2).should().haveText("Mercedes");
